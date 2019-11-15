8a541a889e8f85d4a5df672e65ea3197ed5828db
Fox112345 <Fox112345@yandex.ru>
2019-11-15 19:50:05 +0300

    hbs

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
+const path = require('path')
 const express = require ('express');
 
-const app = express()
+console.log(path.join(__dirname, '../public' ))
 
-app.get('', (req, res)=>{
-    res.send('Hello!')
+const app = express()
+const publicDirPath = path.join(__dirname, '../public' );
+
+app.use(express.static(publicDirPath))
+
+
+app.get('/weather', (req, res)=>{
+    res.send({
+        forecast: 'snowing',
+        location: 'Vld'
+    })
 })
 
 app.listen(3000, () => {
     console.log('Server started');
-    
 })
